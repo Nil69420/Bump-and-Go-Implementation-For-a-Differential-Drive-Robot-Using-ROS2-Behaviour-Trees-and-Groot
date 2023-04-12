cfdb7c5aaf71853964374c535a5fdd7c1c7136e2
Nil <78751630+Nil69420@users.noreply.github.com>
2023-04-12 15:22:41 +0530

    Add files via upload

---
+++ tests/bt_forward_main.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <memory>
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+#include "behaviortree_cpp_v3/utils/shared_library.h"
+#include "behaviortree_cpp_v3/loggers/bt_zmq_publisher.h"
+
+#include "ament_index_cpp/get_package_share_directory.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  auto node = rclcpp::Node::make_shared("forward_node");
+
+  BT::BehaviorTreeFactory factory;
+  BT::SharedLibrary loader;
+
+  factory.registerFromPlugin(loader.getOSName("forward_bt_node"));
+
+  std::string xml_bt =
+    R"(
+    <root main_tree_to_execute = "MainTree" >
+      <BehaviorTree ID="MainTree">
+          <Forward />
+      </BehaviorTree>
+    </root>)";
+
+  auto blackboard = BT::Blackboard::create();
+  blackboard->set("node", node);
+  BT::Tree tree = factory.createTreeFromText(xml_bt, blackboard);
+
+  rclcpp::Rate rate(10);
+  bool finish = false;
+  while (!finish && rclcpp::ok()) {
+    finish = tree.rootNode()->executeTick() != BT::NodeStatus::RUNNING;
+
+    rclcpp::spin_some(node);
+    rate.sleep();
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
